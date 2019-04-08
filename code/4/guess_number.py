28bbc25e699f54c79c22afbf26e50f6760d58d37
Sungchul Choi <sc82.choi@gachon.ac.kr>
2019-04-08 09:44:42 +0900

    add guess_number ENG version

--- code/4/guess_number.py
+++ code/4/guess_number.py
@@ -1,13 +1,13 @@
-import random			# 난수 발생 함수 호출
-guess_number = random.randint(1, 100) 	# 1~100 사이 정수 난수 발생
-print ("숫자를 맞춰보세요 (1 ~ 100)")
-users_input = int(input())	     	# 사용자 입력을 받음
-while (users_input is not guess_number):    	# 사용자 입력과 난수가 같은지 판단
-    if users_input > guess_number: 	# 사용자 입력이 클 경우
-        print ("숫자가 너무 큽니다")
-    else: 		    		# 사용자 입력이 작은 경우
-        print ("숫자가 너무 작습니다")
-    users_input = int(input())	# 다시 사용자 입력을 받음
+import random			# A random number generator function
+guess_number = random.randint(1, 100) 	# Generating integer random number between 1 and 100
+print ("Guess number (1 ~ 100)")
+users_input = int(input())	     	# Conver user input to integer type
+while (users_input is not guess_number):    	# Determine if user input is equal to number of the generated random number
+    if users_input > guess_number: 	# If user input is bigger
+        print ("The number is bigger than random number")
+    else: 		    		# If user input is smaller
+        print ("The number is smaller than random number")
+    users_input = int(input())	# Re-enter user input
 else:
-    print ("정답입니다. ",
-        "입력한 숫자는 ", users_input , "입니다")	# 종료 조건
+    print ("That's true. ",
+        "The input number is", users_input)	# End condition
